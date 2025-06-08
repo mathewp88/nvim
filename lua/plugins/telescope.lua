efe948f0a8c53444231b24661954b119680e5bec
Mathai Mathew Pulicken <49433106+mathewp88@users.noreply.github.com>
2025-06-09 00:45:41 +0530

    Fixed telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,31 +1,47 @@
 return {
-  {
-    'nvim-telescope/telescope.nvim',
-    lazy = true,
-    event = "VimEnter",
-    branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-    end
-  },
-  {
-    'nvim-telescope/telescope-ui-select.nvim',
-    lazy = true,
-    event = "VimEnter",
-    config = function()
-      require("telescope").setup {
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {}
-          }
-        }
-      }
-      require("telescope").load_extension("ui-select")
-    end
-  }
+	{
+		"nvim-telescope/telescope.nvim",
+		lazy = true,
+		event = "VimEnter",
+		branch = "0.1.x",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", function()
+				builtin.find_files({
+					hidden = true,
+					file_ignore_patterns = { ".git/" },
+				})
+			end, {})
+			vim.keymap.set("n", "<leader>fg", function()
+				builtin.live_grep({
+					additional_args = function()
+						return {
+							"--hidden", -- Search hidden files
+							"--no-ignore", -- Include .gitignored files
+							"--glob",
+							"!.git/*", -- Exclude .git directory
+						}
+					end,
+				})
+			end, {})
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		lazy = true,
+		event = "VimEnter",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
