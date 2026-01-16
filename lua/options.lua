e8e297f30581154160b5fd95cb3278fc9ce3424d
Mathai Mathew <49433106+mathewp88@users.noreply.github.com>
2026-01-16 14:15:10 +0530

    Removing some options

--- lua/options.lua
+++ lua/options.lua
@@ -103,9 +103,9 @@ vim.keymap.set("n", "<leader>rpl", function()
 end, { desc = "ROS: List Parameters" })
 
 -- For Learning
-vim.keymap.set("", "<up>", "<nop>", { noremap = true })
-vim.keymap.set("", "<down>", "<nop>", { noremap = true })
-vim.keymap.set("i", "<up>", "<nop>", { noremap = true })
-vim.keymap.set("i", "<down>", "<nop>", { noremap = true })
+-- vim.keymap.set("", "<up>", "<nop>", { noremap = true })
+-- vim.keymap.set("", "<down>", "<nop>", { noremap = true })
+-- vim.keymap.set("i", "<up>", "<nop>", { noremap = true })
+-- vim.keymap.set("i", "<down>", "<nop>", { noremap = true })
 
 vim.opt.mouse = ""
